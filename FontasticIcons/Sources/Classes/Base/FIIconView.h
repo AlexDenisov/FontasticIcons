c466b9dd0867f708b44b15dcd6629ee77f3b1a0d
Alex Denisov <1101.debian@gmail.com>
2012-10-28 22:01:42 +0200

    Restructured

---
+++ FontasticIcons/Sources/Classes/Base/FIIconView.h
@@ -0,0 +1,16 @@
+//
+//  FIIconView.h
+//  FontasticIcons
+//
+//  Created by Alex Denisov on 28.10.12.
+//  Copyright (c) 2012 Alex Denisov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FIIconView : UIView
+
+@property (nonatomic, readwrite) CGFloat padding;
+@property (nonatomic, strong) UIColor *iconColor;
+
+@end
---
+++ FontasticIcons/Sources/Classes/Base/FIIconView.m
@@ -0,0 +1,13 @@
+//
+//  FIIconView.m
+//  FontasticIcons
+//
+//  Created by Alex Denisov on 28.10.12.
+//  Copyright (c) 2012 Alex Denisov. All rights reserved.
+//
+
+#import "FIIconView.h"
+
+@implementation FIIconView
+
+@end
