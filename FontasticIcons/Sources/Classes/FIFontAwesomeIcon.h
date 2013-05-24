726326e2dd1deed733513b0ae64395aad8d9a3c5
Alex Denisov <1101.debian@gmail.com>
2013-05-24 09:12:42 +0300

    FontAwesome updated to match new version

--- FontasticIcons/Sources/Classes/FIFontAwesomeIcon.h
+++ FontasticIcons/Sources/Classes/FIFontAwesomeIcon.h
@@ -13,215 +13,609 @@
 
 @interface FIFontAwesomeIcon (Forwarding)
 
-+ (instancetype)glassIcon;
-+ (instancetype)musicIcon;
-+ (instancetype)searchIcon;
-+ (instancetype)envelopeIcon;
-+ (instancetype)envelopeAltIcon;
-+ (instancetype)heartIcon;
-+ (instancetype)heartEmptyIcon;
-+ (instancetype)starIcon;
-+ (instancetype)starEmptyIcon;
-+ (instancetype)starHalfIcon;
-+ (instancetype)userIcon;
-+ (instancetype)groupIcon;
-+ (instancetype)userMdIcon;
-+ (instancetype)filmIcon;
-+ (instancetype)facetimeVideoIcon;
-+ (instancetype)pictureIcon;
-+ (instancetype)cameraIcon;
-+ (instancetype)cameraRetroIcon;
-+ (instancetype)thLargeIcon;
-+ (instancetype)thIcon;
-+ (instancetype)thListIcon;
-+ (instancetype)okIcon;
-+ (instancetype)okSignIcon;
-/*! @seealso renamed okSignIcon in 0.3.0 */+ (instancetype)okCircleIcon;
-+ (instancetype)removeIcon;
-+ (instancetype)removeCircleIcon;
-+ (instancetype)removeSignIcon;
-+ (instancetype)plusIcon;
-+ (instancetype)plusSignIcon;
-+ (instancetype)minusIcon;
-+ (instancetype)minusSignIcon;
-+ (instancetype)questionSignIcon;
-+ (instancetype)infoSignIcon;
-+ (instancetype)homeIcon;
-+ (instancetype)linkIcon;
-+ (instancetype)externalLinkIcon;
-+ (instancetype)paperClipIcon;
-+ (instancetype)lockIcon;
-+ (instancetype)unlockIcon;
-+ (instancetype)pushpinIcon;
-+ (instancetype)eyeOpenIcon;
-+ (instancetype)eyeCloseIcon;
-+ (instancetype)tagIcon;
-+ (instancetype)tagsIcon;
-+ (instancetype)bookmarkIcon;
-+ (instancetype)bookmarkEmptyIcon;
-+ (instancetype)flagIcon;
-+ (instancetype)thumbsUpIcon;
-+ (instancetype)thumbsDownIcon;
-+ (instancetype)downloadAltIcon;
-+ (instancetype)uploadAltIcon;
-+ (instancetype)shareAltIcon;
-+ (instancetype)shareIcon;
-+ (instancetype)pencilIcon;
-+ (instancetype)editIcon;
-+ (instancetype)printIcon;
-+ (instancetype)retweetIcon;
-/*! @seealso renamed commentAltIcon in 0.3.0 */+ (instancetype)commentIcon;
-+ (instancetype)commentsIcon;
-+ (instancetype)commentAltIcon;
-+ (instancetype)commentsAltIcon;
-+ (instancetype)bellIcon;
-+ (instancetype)warningSignIcon;
-+ (instancetype)exclamationSignIcon;
-+ (instancetype)mapMarkerIcon;
-+ (instancetype)trashIcon;
-+ (instancetype)fileIcon;
-+ (instancetype)copyIcon;
-+ (instancetype)folderCloseIcon;
-+ (instancetype)folderOpenIcon;
-+ (instancetype)rssIcon;
-+ (instancetype)phoneIcon;
-+ (instancetype)phoneSignIcon;
-+ (instancetype)reorderIcon;
-+ (instancetype)cogIcon;
-+ (instancetype)cogsIcon;
-+ (instancetype)wrenchIcon;
-+ (instancetype)shoppingCartIcon;
-+ (instancetype)calendarIcon;
-+ (instancetype)signinIcon;
-+ (instancetype)signoutIcon;
-+ (instancetype)volumeOffIcon;
-+ (instancetype)volumeDownIcon;
-+ (instancetype)volumeUpIcon;
-+ (instancetype)headphonesIcon;
-+ (instancetype)timeIcon;
-+ (instancetype)banCircleIcon;
-+ (instancetype)resizeFullIcon;
-+ (instancetype)fullscreenIcon;
-+ (instancetype)resizeSmallIcon;
-+ (instancetype)resizeVerticalIcon;
-+ (instancetype)resizeHorizontalIcon;
-+ (instancetype)moveIcon;
-+ (instancetype)zoomInIcon;
-+ (instancetype)zoomOutIcon;
-/*! @seealso downloadAltIcon renamed in 0.3.0 */+ (instancetype)downloadIcon;
-/*! @seealso uploadAltIcon renamed in 0.3.0 */+ (instancetype)uploadIcon;
-+ (instancetype)caretDownIcon;
-+ (instancetype)caretUpIcon;
-+ (instancetype)caretLeftIcon;
-+ (instancetype)caretRightIcon;
-+ (instancetype)chevronDownIcon;
-+ (instancetype)chevronLeftIcon;
-+ (instancetype)chevronRightIcon;
-+ (instancetype)chevronUpIcon;
-+ (instancetype)arrowDownIcon;
-+ (instancetype)arrowLeftIcon;
-+ (instancetype)arrowRightIcon;
-+ (instancetype)arrowUpIcon;
-+ (instancetype)handRightIcon;
-+ (instancetype)handLeftIcon;
-+ (instancetype)handUpIcon;
-+ (instancetype)handDownIcon;
-+ (instancetype)circleArrowLeftIcon;
-+ (instancetype)circleArrowRightIcon;
-+ (instancetype)circleArrowUpIcon;
-+ (instancetype)circleArrowDownIcon;
-+ (instancetype)repeatIcon;
-+ (instancetype)undoIcon;
-+ (instancetype)refreshIcon;
-+ (instancetype)randomIcon;
-+ (instancetype)playIcon;
-+ (instancetype)playCircleIcon;
-+ (instancetype)stopIcon;
-+ (instancetype)pauseIcon;
-+ (instancetype)stepForwardIcon;
-+ (instancetype)fastForwardIcon;
-+ (instancetype)stepBackwardIcon;
-+ (instancetype)fastBackwardIcon;
-/*! @seealso renamed shareAltIcon in 0.3.0 */+ (instancetype)forwardIcon;
-+ (instancetype)backwardIcon;
-+ (instancetype)ejectIcon;
-+ (instancetype)screenshotIcon;
-+ (instancetype)signalIcon;
-+ (instancetype)trophyIcon;
-+ (instancetype)inboxIcon;
-+ (instancetype)globeIcon;
-+ (instancetype)cloudIcon;
-+ (instancetype)boltIcon;
-+ (instancetype)umbrellaIcon;
-+ (instancetype)planeIcon;
-+ (instancetype)leafIcon;
-+ (instancetype)fontIcon;
-+ (instancetype)boldIcon;
-+ (instancetype)italicIcon;
-+ (instancetype)textHeightIcon;
-+ (instancetype)textWidthIcon;
-+ (instancetype)alignLeftIcon;
-+ (instancetype)alignCenterIcon;
-+ (instancetype)alignRightIcon;
-+ (instancetype)alignJustifyIcon;
-+ (instancetype)listIcon;
-+ (instancetype)indentLeftIcon;
-+ (instancetype)indentRightIcon;
-+ (instancetype)listUlIcon;
-+ (instancetype)listOlIcon;
-+ (instancetype)strikethroughIcon;
-+ (instancetype)underlineIcon;
-+ (instancetype)tableIcon;
-+ (instancetype)columnsIcon;
-+ (instancetype)cutIcon;
-+ (instancetype)pasteIcon;
-+ (instancetype)briefcaseIcon;
-+ (instancetype)offIcon;
-+ (instancetype)roadIcon;
-+ (instancetype)listAltIcon;
-+ (instancetype)qrcodeIcon;
-+ (instancetype)barcodeIcon;
-+ (instancetype)bookIcon;
-+ (instancetype)adjustIcon;
-+ (instancetype)tintIcon;
-+ (instancetype)checkIcon;
-+ (instancetype)checkEmptyIcon;
-+ (instancetype)asteriskIcon;
-+ (instancetype)giftIcon;
-+ (instancetype)fireIcon;
-+ (instancetype)magnetIcon;
-+ (instancetype)barChartIcon;
-+ (instancetype)creditCardIcon;
-+ (instancetype)saveIcon;
-+ (instancetype)bullhornIcon;
-+ (instancetype)hddIcon;
-+ (instancetype)keyIcon;
-+ (instancetype)certificateIcon;
-+ (instancetype)tasksIcon;
-+ (instancetype)filterIcon;
-+ (instancetype)beakerIcon;
-+ (instancetype)magicIcon;
-+ (instancetype)truckIcon;
-+ (instancetype)moneyIcon;
-+ (instancetype)sortIcon;
-+ (instancetype)sortDownIcon;
-+ (instancetype)sortUpIcon;
-+ (instancetype)legalIcon;
-+ (instancetype)gaugeIcon;
-+ (instancetype)sitemapIcon;
-+ (instancetype)githubSignIcon;
-+ (instancetype)githubIcon;
-+ (instancetype)twitterSignIcon;
-+ (instancetype)twitterIcon;
-+ (instancetype)facebookSignIcon;
-+ (instancetype)facebookIcon;
-+ (instancetype)linkedinSignIcon;
-+ (instancetype)linkedinIcon;
-+ (instancetype)pinterestIcon;
-+ (instancetype)pinterestSignIcon;
-+ (instancetype)googlePlusSignIcon;
-+ (instancetype)googlePlusIcon;
-+ (instancetype)signBlankIcon;
-+ (instancetype)lemonIcon;
+- (instancetype)glass;
+
+- (instancetype)music;
+
+- (instancetype)search;
+
+- (instancetype)envelope;
+
+- (instancetype)heart;
+
+- (instancetype)star;
+
+- (instancetype)starEmpty;
+
+- (instancetype)user;
+
+- (instancetype)film;
+
+- (instancetype)thLarge;
+
+- (instancetype)th;
+
+- (instancetype)thList;
+
+- (instancetype)ok;
+
+- (instancetype)remove;
+
+- (instancetype)zoomIn;
+
+- (instancetype)zoomOut;
+
+- (instancetype)off;
+
+- (instancetype)signal;
+
+- (instancetype)cog;
+
+- (instancetype)trash;
+
+- (instancetype)home;
+
+- (instancetype)file;
+
+- (instancetype)time;
+
+- (instancetype)road;
+
+- (instancetype)downloadAlt;
+
+- (instancetype)download;
+
+- (instancetype)upload;
+
+- (instancetype)inbox;
+
+- (instancetype)playCircle;
+
+- (instancetype)repeat;
+
+- (instancetype)refresh;
+
+- (instancetype)listAlt;
+
+- (instancetype)lock;
+
+- (instancetype)flag;
+
+- (instancetype)headphones;
+
+- (instancetype)volumeOff;
+
+- (instancetype)volumeDown;
+
+- (instancetype)volumeUp;
+
+- (instancetype)qrcode;
+
+- (instancetype)barcode;
+
+- (instancetype)tag;
+
+- (instancetype)tags;
+
+- (instancetype)book;
+
+- (instancetype)bookmark;
+
+- (instancetype)print;
+
+- (instancetype)camera;
+
+- (instancetype)font;
+
+- (instancetype)bold;
+
+- (instancetype)italic;
+
+- (instancetype)textHeight;
+
+- (instancetype)textWidth;
+
+- (instancetype)alignLeft;
+
+- (instancetype)alignCenter;
+
+- (instancetype)alignRight;
+
+- (instancetype)alignJustify;
+
+- (instancetype)list;
+
+- (instancetype)indentLeft;
+
+- (instancetype)indentRight;
+
+- (instancetype)facetimeVideo;
+
+- (instancetype)picture;
+
+- (instancetype)pencil;
+
+- (instancetype)mapMarker;
+
+- (instancetype)adjust;
+
+- (instancetype)tint;
+
+- (instancetype)edit;
+
+- (instancetype)share;
+
+- (instancetype)check;
+
+- (instancetype)move;
+
+- (instancetype)stepBackward;
+
+- (instancetype)fastBackward;
+
+- (instancetype)backward;
+
+- (instancetype)play;
+
+- (instancetype)pause;
+
+- (instancetype)stop;
+
+- (instancetype)forward;
+
+- (instancetype)fastForward;
+
+- (instancetype)stepForward;
+
+- (instancetype)eject;
+
+- (instancetype)chevronLeft;
+
+- (instancetype)chevronRight;
+
+- (instancetype)plusSign;
+
+- (instancetype)minusSign;
+
+- (instancetype)removeSign;
+
+- (instancetype)okSign;
+
+- (instancetype)questionSign;
+
+- (instancetype)infoSign;
+
+- (instancetype)screenshot;
+
+- (instancetype)removeCircle;
+
+- (instancetype)okCircle;
+
+- (instancetype)banCircle;
+
+- (instancetype)arrowLeft;
+
+- (instancetype)arrowRight;
+
+- (instancetype)arrowUp;
+
+- (instancetype)arrowDown;
+
+- (instancetype)shareAlt;
+
+- (instancetype)resizeFull;
+
+- (instancetype)resizeSmall;
+
+- (instancetype)plus;
+
+- (instancetype)minus;
+
+- (instancetype)asterisk;
+
+- (instancetype)exclamationSign;
+
+- (instancetype)gift;
+
+- (instancetype)leaf;
+
+- (instancetype)fire;
+
+- (instancetype)eyeOpen;
+
+- (instancetype)eyeClose;
+
+- (instancetype)warningSign;
+
+- (instancetype)plane;
+
+- (instancetype)calendar;
+
+- (instancetype)random;
+
+- (instancetype)comment;
+
+- (instancetype)magnet;
+
+- (instancetype)chevronUp;
+
+- (instancetype)chevronDown;
+
+- (instancetype)retweet;
+
+- (instancetype)shoppingCart;
+
+- (instancetype)folderClose;
+
+- (instancetype)folderOpen;
+
+- (instancetype)resizeVertical;
+
+- (instancetype)resizeHorizontal;
+
+- (instancetype)barChart;
+
+- (instancetype)twitterSign;
+
+- (instancetype)facebookSign;
+
+- (instancetype)cameraRetro;
+
+- (instancetype)key;
+
+- (instancetype)cogs;
+
+- (instancetype)comments;
+
+- (instancetype)thumbsUp;
+
+- (instancetype)thumbsDown;
+
+- (instancetype)starHalf;
+
+- (instancetype)heartEmpty;
+
+- (instancetype)signout;
+
+- (instancetype)linkedinSign;
+
+- (instancetype)pushpin;
+
+- (instancetype)externalLink;
+
+- (instancetype)signin;
+
+- (instancetype)trophy;
+
+- (instancetype)githubSign;
+
+- (instancetype)uploadAlt;
+
+- (instancetype)lemon;
+
+- (instancetype)phone;
+
+- (instancetype)checkEmpty;
+
+- (instancetype)bookmarkEmpty;
+
+- (instancetype)phoneSign;
+
+- (instancetype)twitter;
+
+- (instancetype)facebook;
+
+- (instancetype)github;
+
+- (instancetype)unlock;
+
+- (instancetype)creditCard;
+
+- (instancetype)rss;
+
+- (instancetype)hdd;
+
+- (instancetype)bullhorn;
+
+- (instancetype)bell;
+
+- (instancetype)certificate;
+
+- (instancetype)handRight;
+
+- (instancetype)handLeft;
+
+- (instancetype)handUp;
+
+- (instancetype)handDown;
+
+- (instancetype)circleArrowLeft;
+
+- (instancetype)circleArrowRight;
+
+- (instancetype)circleArrowUp;
+
+- (instancetype)circleArrowDown;
+
+- (instancetype)globe;
+
+- (instancetype)wrench;
+
+- (instancetype)tasks;
+
+- (instancetype)filter;
+
+- (instancetype)briefcase;
+
+- (instancetype)fullscreen;
+
+- (instancetype)group;
+
+- (instancetype)link;
+
+- (instancetype)cloud;
+
+- (instancetype)beaker;
+
+- (instancetype)cut;
+
+- (instancetype)copy;
+
+- (instancetype)paperClip;
+
+- (instancetype)save;
+
+- (instancetype)signBlank;
+
+- (instancetype)reorder;
+
+- (instancetype)listUl;
+
+- (instancetype)listOl;
+
+- (instancetype)strikethrough;
+
+- (instancetype)underline;
+
+- (instancetype)table;
+
+- (instancetype)magic;
+
+- (instancetype)truck;
+
+- (instancetype)pinterest;
+
+- (instancetype)pinterestSign;
+
+- (instancetype)googlePlusSign;
+
+- (instancetype)googlePlus;
+
+- (instancetype)money;
+
+- (instancetype)caretDown;
+
+- (instancetype)caretUp;
+
+- (instancetype)caretLeft;
+
+- (instancetype)caretRight;
+
+- (instancetype)columns;
+
+- (instancetype)sort;
+
+- (instancetype)sortDown;
+
+- (instancetype)sortUp;
+
+- (instancetype)envelopeAlt;
+
+- (instancetype)linkedin;
+
+- (instancetype)undo;
+
+- (instancetype)legal;
+
+- (instancetype)dashboard;
+
+- (instancetype)commentAlt;
+
+- (instancetype)commentsAlt;
+
+- (instancetype)bolt;
+
+- (instancetype)sitemap;
+
+- (instancetype)umbrella;
+
+- (instancetype)paste;
+
+- (instancetype)lightbulb;
+
+- (instancetype)exchange;
+
+- (instancetype)cloudDownload;
+
+- (instancetype)cloudUpload;
+
+- (instancetype)userMd;
+
+- (instancetype)stethoscope;
+
+- (instancetype)suitcase;
+
+- (instancetype)bellAlt;
+
+- (instancetype)coffee;
+
+- (instancetype)food;
+
+- (instancetype)fileAlt;
+
+- (instancetype)building;
+
+- (instancetype)hospital;
+
+- (instancetype)ambulance;
+
+- (instancetype)medkit;
+
+- (instancetype)fighterJet;
+
+- (instancetype)beer;
+
+- (instancetype)hSign;
+
+- (instancetype)plusSignAlt;
+
+- (instancetype)doubleAngleLeft;
+
+- (instancetype)doubleAngleRight;
+
+- (instancetype)doubleAngleUp;
+
+- (instancetype)doubleAngleDown;
+
+- (instancetype)angleLeft;
+
+- (instancetype)angleRight;
+
+- (instancetype)angleUp;
+
+- (instancetype)angleDown;
+
+- (instancetype)desktop;
+
+- (instancetype)laptop;
+
+- (instancetype)tablet;
+
+- (instancetype)mobilePhone;
+
+- (instancetype)circleBlank;
+
+- (instancetype)quoteLeft;
+
+- (instancetype)quoteRight;
+
+- (instancetype)spinner;
+
+- (instancetype)circle;
+
+- (instancetype)reply;
+
+- (instancetype)folderCloseAlt;
+
+- (instancetype)folderOpenAlt;
+
+- (instancetype)expandAlt;
+
+- (instancetype)collapseAlt;
+
+- (instancetype)smile;
+
+- (instancetype)frown;
+
+- (instancetype)meh;
+
+- (instancetype)gamepad;
+
+- (instancetype)keyboard;
+
+- (instancetype)flagAlt;
+
+- (instancetype)flagCheckered;
+
+- (instancetype)terminal;
+
+- (instancetype)code;
+
+- (instancetype)replyAll;
+
+- (instancetype)mailReplyAll;
+
+- (instancetype)starHalfEmpty;
+
+- (instancetype)locationArrow;
+
+- (instancetype)crop;
+
+- (instancetype)codeFork;
+
+- (instancetype)unlink;
+
+- (instancetype)question;
+
+- (instancetype)info;
+
+- (instancetype)exclamation;
+
+- (instancetype)superscript;
+
+- (instancetype)subscript;
+
+- (instancetype)eraser;
+
+- (instancetype)puzzlePiece;
+
+- (instancetype)microphone;
+
+- (instancetype)microphoneOff;
+
+- (instancetype)shield;
+
+- (instancetype)calendarEmpty;
+
+- (instancetype)fireExtinguisher;
+
+- (instancetype)rocket;
+
+- (instancetype)maxcdn;
+
+- (instancetype)chevronSignLeft;
+
+- (instancetype)chevronSignRight;
+
+- (instancetype)chevronSignUp;
+
+- (instancetype)chevronSignDown;
+
+- (instancetype)html5;
+
+- (instancetype)css3;
+
+- (instancetype)anchor;
+
+- (instancetype)unlockAlt;
+
+- (instancetype)bullseye;
+
+- (instancetype)ellipsisHorizontal;
+
+- (instancetype)ellipsisVertical;
+
+- (instancetype)rssSign;
+
+- (instancetype)playSign;
+
+- (instancetype)ticket;
+
+- (instancetype)minusSignAlt;
+
+- (instancetype)checkMinus;
+
+- (instancetype)levelUp;
+
+- (instancetype)levelDown;
+
+- (instancetype)checkSign;
+
+- (instancetype)editSign;
+
+- (instancetype)externalLinkSign;
+
+- (instancetype)shareSign;
+
 
 @end
